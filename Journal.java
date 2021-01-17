f19a8efa7d3b16e0a5e8bd11f072beb0c199ab9b
Tobias Ephron <tobiasephron@gmail.com>
2021-01-17 12:47:17 -0500

    Program is a menu that shows the questions and your answers (if any). Answers can be changed (menu is traversable)

--- Journal.java
+++ Journal.java
@@ -12,28 +12,121 @@ public class Journal{
 
 	String[] questions = {"How was your day? (1-10)", "Did you code today?"};
 	private String[] answers;
+	Scanner scan;
 
 	public Journal(){
 		this.questions = questions;
-		this.answers = new String[questions.length];
+		this.answers = new String[this.questions.length];
+		for(int i=0;i<this.answers.length;i++){
+			this.answers[i] = " ";
+		}
+		this.scan = new Scanner(System.in);
 	}
 
 	public static void main(String[] args){
 		Journal j = new Journal();
-		Scanner s = new Scanner(System.in);
 
 		System.out.println("");
 		System.out.println("Hello");
-		String ans = s.nextLine();
+		String ans = j.scan.nextLine();
 
-		j.askQuestions();
+		//j.printQuestions();
+
+		//j.printDisplay();
+
+		j.menu();
+
+
 	}
 
+	public void menu(){
+		String ques = "-1";
+		int quesNum = -1;
+		while(true){
+			this.printDisplay(true);
+			
+			System.out.println("What would you like to answer/change (give number)?");
+			
+			ques = "-1";
+			quesNum = -1;
+			
+			boolean change = false;
+			//boolean validIn = false;
 
-	public void askQuestions(){
-		this.clearScreen();
+			while(Integer.parseInt(ques) < 0){
+				try{
+					ques = this.scan.nextLine();
+
+					if(ques.toLowerCase().equals("none") ||  ques.toLowerCase().equals("quit")){
+						return;
+					}
+					else{
+						quesNum = Integer.parseInt(ques) - 1;
+						ques = Integer.toString(quesNum);
+					}
+					
+					change = true;
+					//validIn = true;
+				} catch(NumberFormatException e){
+					change = false;
+					break;
+					//validIn = false;
+				}
+			}
+			if(!change /**&& !validIn*/ || quesNum >= this.questions.length){
+				System.out.println("");
+				System.out.println("Invalid input");
+				System.out.println("");
+				System.out.println("Try again? (y/n)");
+				System.out.println("");
+				
+				String ans = this.scan.nextLine();
+				
+				if(ans.toLowerCase().charAt(0) == 'y')
+					continue;
+				else{
+					return;
+				}
+			}
+			else{
+				this.clearScreen();
+
+				System.out.println("");
+				System.out.println(quesNum+1 + " - " + this.questions[quesNum] + " -");
+				System.out.println("---------------------------");
+				System.out.println("");
+				this.answers[quesNum] = this.scan.nextLine();
+			}
+		}
+	}
+
+	public void printDisplay(boolean clear){
+		if(clear) this.clearScreen();
+
 		System.out.println("");
-		System.out.print("Questions");
+		System.out.println("Your Journal:");
+		System.out.println("");
+		
+		for(int i = 0; i < this.questions.length; i++){
+			System.out.println(i+1  + " - " + this.questions[i] + " -");
+			System.out.println("------------------------------");
+			try{
+				System.out.println("  " + this.answers[i]);
+			} catch(ArrayIndexOutOfBoundsException ex){
+				System.out.println("");
+			}
+			System.out.println("");
+		}
+	}
+
+	public void printQuestions(){
+		//this.clearScreen();
+		System.out.println("");
+		System.out.print("Questions:");
+		System.out.println("");
+		for(String q: this.questions){
+			System.out.println("- " + q);
+		}
 		System.out.println("");
 	}
 
