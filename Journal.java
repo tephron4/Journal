e5dbeb617cf3fe1a475bb21de59c6b4a1cc37522
Tobias Ephron <tobiasephron@gmail.com>
2021-01-18 16:05:17 -0500

    Program now asks for the date and checks for partial validity

--- Journal.java
+++ Journal.java
@@ -6,13 +6,23 @@
  */
 
 import java.util.*;
+import java.io.File;
 import java.io.IOException;
 
 public class Journal{
 
 	String[] questions = {"How was your day? (1-10)", "Did you code today?", "Did you read today and what did you read?", "Workout?", "How much water did you drink?"};
 	private String[] answers;
+	String date;
+
 	Scanner scan;
+
+	String[] months = {"january", "jan", "1", "01", "february", "feb", "2", "02",
+						"march", "mar", "3", "03", "april", "apr", "4", "04",
+						"may", "5", "05", "june", "jun", "6", "06", "july",
+						"jul", "7", "07", "august", "aug", "8", "08", 
+						"september", "sept", "sep", "9", "09", "october", "oct", "10",
+						"november", "nov", "11", "december", "dec", "12"};
 
 	public Journal(){
 		this.questions = questions;
@@ -20,7 +30,18 @@ public class Journal{
 		for(int i=0;i<this.answers.length;i++){
 			this.answers[i] = " ";
 		}
+
 		this.scan = new Scanner(System.in);
+
+		while(true){
+			try{
+				this.date = this.getDate();
+				if(this.date.equals("quit")) System.exit(0);
+			} catch(InterruptedException e){
+				continue;
+			}
+			break;
+		}
 	}
 
 	public static void main(String[] args){
@@ -35,8 +56,95 @@ public class Journal{
 		//j.printDisplay();
 
 		j.menu();
+	}
 
+	public String getDate() throws InterruptedException{
+		this.clearScreen();
+		
+		String d = "";
+		System.out.println("");
 
+		while(d.equals("")){
+			System.out.println("What's the date? (Month/Day/Year)");
+			d = this.scan.nextLine().toLowerCase();
+			if(d.equals("quit")) break;
+
+			d = this.parseDate(d);
+
+			if(d.equals("invalid")){
+				this.clearScreen();
+				System.out.println(" \" " + d + "\" is an invalid date.");
+				Thread.sleep(5000);
+				d = "";
+				this.clearScreen();
+				continue;
+			}
+		}
+
+		return d;
+	}
+
+	public String parseDate(String date){
+		String month = "";
+		String day = "";
+		String year = "";
+
+		int track = 1;
+		for(int i = 0; i < date.length(); i++){
+			if(track == 1){
+				if(date.charAt(i) == '/'){
+					track = 2;
+					continue;
+				}
+				else{
+					month += date.charAt(i);
+				}
+			}
+			else if(track == 2){
+				if(date.charAt(i) == '/'){
+					track = 3;
+					continue;
+				}
+				else{
+					day += date.charAt(i);
+				}
+			}
+			else if(track == 3){
+				if(date.charAt(i) == '/'){
+					track = 1;
+					continue;
+				}
+				else{
+					year += date.charAt(i);
+				}
+			}
+		}
+
+		if(this.checkDate(month, day, year)){
+			return month + day + year;
+		}
+		
+		return "invalid";
+
+	}
+
+	public boolean checkDate(String m, String d, String y){
+		int day = -1;
+		if(Arrays.asList(months).contains(m)){
+			try{
+				day = Integer.parseInt(d);
+			} catch(NumberFormatException e){
+				return false;
+			}
+			if(!(0 < day && day <= 31)) return false;
+			try{
+				int year = Integer.parseInt(y);
+			} catch(NumberFormatException e){
+				return false;
+			}
+			return true;
+		}
+		else return false;
 	}
 
 	public void menu(){
