120a3cbfe88e140836b671db96d08ecf89b72e7f
Tobias Ephron <tobiasephron@gmail.com>
2021-02-17 10:08:55 -0500

    Alterations to Journal.java code (got rid of unnecessary code)

--- Journal.java
+++ Journal.java
@@ -20,8 +20,6 @@ public class Journal{
 	private File entry;
 	private FileWriter writer;
 
-	Scanner scan;
-
 	String[] months = {"january", "jan", "1", "01", "february", "feb", "2", "02",
 						"march", "mar", "3", "03", "april", "apr", "4", "04",
 						"may", "5", "05", "june", "jun", "6", "06", "july",
@@ -33,30 +31,12 @@ public class Journal{
 	String dash = "-";
 
 	public Journal(){
+
 		this.questions = questions;
 		this.answers = new String[this.questions.length];
 		for(int i=0;i<this.answers.length;i++){
 			this.answers[i] = " ";
 		}
-
-		this.scan = new Scanner(System.in);
-
-		while(true){
-			try{
-				this.date = this.getDate();
-				if(this.date.equals("quit")) System.exit(0);
-				this.filePath = "C:\\Users\\tobia\\JournalEntries\\" + this.date + "journal.txt";
-			} catch(InterruptedException e){
-				continue;
-			}
-			
-			if(!this.makeFile()){
-				continue;
-			}
-
-			break;
-		}
-
 
 	}
 
@@ -70,8 +50,6 @@ public class Journal{
 		//j.printQuestions();
 
 		//j.printDisplay();
-
-		j.menu();
 
 		j.writeToFile();
 	}
@@ -153,32 +131,6 @@ public class Journal{
 		}
 	}
 
-	public String getDate() throws InterruptedException{
-		this.clearScreen();
-		
-		String d = "";
-		System.out.println("");
-
-		while(d.equals("")){
-			System.out.println("What's the date? (Month/Day/Year)");
-			d = this.scan.nextLine().toLowerCase();
-			if(d.equals("quit")) break;
-
-			d = this.parseDate(d);
-
-			if(d.equals("invalid")){
-				this.clearScreen();
-				System.out.println(" \" " + d + "\" is an invalid date.");
-				Thread.sleep(5000);
-				d = "";
-				this.clearScreen();
-				continue;
-			}
-		}
-
-		return d;
-	}
-
 	public String parseDate(String date){
 		String month = "";
 		String day = "";
@@ -242,84 +194,8 @@ public class Journal{
 		else return false;
 	}
 
-	public void menu(){
-		String ques = "-1";
-		int quesNum = -1;
-		while(true){
-			this.printDisplay(true);
-			
-			System.out.println("What would you like to answer/change (give number)?");
-			
-			ques = "-1";
-			quesNum = -1;
-			
-			boolean change = false;
-			//boolean validIn = false;
-
-			while(Integer.parseInt(ques) < 0){
-				try{
-					ques = this.scan.nextLine();
-
-					if(ques.toLowerCase().equals("none") ||  ques.toLowerCase().equals("quit")){
-						return;
-					}
-					else{
-						quesNum = Integer.parseInt(ques) - 1;
-						ques = Integer.toString(quesNum);
-					}
-					
-					change = true;
-					//validIn = true;
-				} catch(NumberFormatException e){
-					change = false;
-					break;
-					//validIn = false;
-				}
-			}
-			if(!change /**&& !validIn*/ || quesNum >= this.questions.length){
-				System.out.println("");
-				System.out.println("Invalid input");
-				System.out.println("");
-				System.out.println("Try again? (y/n)");
-				System.out.println("");
-				
-				String ans = this.scan.nextLine();
-				
-				if(ans.toLowerCase().charAt(0) == 'y')
-					continue;
-				else{
-					return;
-				}
-			}
-			else{
-				this.clearScreen();
-
-				System.out.println("");
-				System.out.println(quesNum+1 + " - " + this.questions[quesNum] + " -");
-				System.out.println("---------------------------");
-				System.out.println("");
-				this.answers[quesNum] = this.scan.nextLine();
-			}
-		}
-	}
-
-	public void printDisplay(boolean clear){
-		if(clear) this.clearScreen();
-
-		System.out.println("");
-		System.out.println("Your " + this.date + " Journal:");
-		System.out.println("");
-		
-		for(int i = 0; i < this.questions.length; i++){
-			System.out.println(i+1  + " - " + this.questions[i] + " -");
-			System.out.println("------------------------------");
-			try{
-				System.out.println("  " + this.answers[i]);
-			} catch(ArrayIndexOutOfBoundsException ex){
-				System.out.println("");
-			}
-			System.out.println("");
-		}
+	public void setAnswers(String[] a){
+		this.answers = a;
 	}
 
 	public void printQuestions(){
