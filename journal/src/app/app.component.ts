79323349bba0e03ecbe28d7df3e299dfb5ed2c1e
Tobias Ephron <tobiasephron@gmail.com>
2023-11-25 19:09:59 -0700

    Created day component that displays date.
    Day component displays and calendar component is hidden when date is
    selected on calendar. Date is correctly sent to the day component and
    displayed.
    
    TEST=Ran app locally to test display and date correctness.

--- journal/src/app/app.component.ts
+++ journal/src/app/app.component.ts
@@ -1,16 +1,40 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
 import { CalendarComponent } from './calendar/calendar.component';
+import { DayComponent } from './day/day.component';
+import { HeaderComponent } from './header/header.component';
+
+export interface DateType {
+  month: string;
+  day: string;
+  year: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CalendarComponent, CommonModule, HeaderComponent, RouterOutlet],
+  imports: [
+    CalendarComponent, 
+    CommonModule, 
+    DayComponent, 
+    HeaderComponent, 
+    RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'journal';
+
+  showDay = false;
+  date?: DateType;
+
+  changeDay(newDate: DateType) {
+    this.date = newDate;
+    this.toggleDayView(true);
+  }
+
+  toggleDayView(show: boolean) {
+    this.showDay = show;
+  }
 }
