3ba09d40fec9fdd0bd127c8dccaf110c86a576f9
Tobias Ephron <tobiasephron@gmail.com>
2023-11-27 14:09:20 -0700

    Add to-do textarea to day component.

---
+++ journal/src/app/todo/todo.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+@Component({
+  selector: 'app-todo',
+  standalone: true,
+  imports: [CommonModule, MatInputModule, MatFormFieldModule],
+  templateUrl: './todo.component.html',
+  styleUrl: './todo.component.css'
+})
+export class TodoComponent {
+
+}
